4a27c51ace6bdc6261218aa602629befbf6fefd7
runroob <2658085067@qq.com>
2023-07-18 12:23:25 +0800

    Initial commit

---
+++ src/main/java/selab/desktop/resource_management/ResourceManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package selab.desktop.resource_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceManagementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/selab/desktop/resource_management/ResourceManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package selab.desktop.resource_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceManagementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
