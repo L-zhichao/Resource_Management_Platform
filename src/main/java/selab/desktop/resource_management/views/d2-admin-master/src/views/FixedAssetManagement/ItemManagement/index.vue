8b44f1d8ffdae87efbb09231c9f89ad4faf7ede6
XunJi <329453626x@gmail.com>
2023-07-22 19:31:18 +0800

    前端框架+登录注册+物品管理(未完成)
    登录用的mock模拟返回所以可用，注册不可用

---
+++ src/main/java/selab/desktop/resource_management/views/d2-admin-master/src/views/FixedAssetManagement/ItemManagement/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <d2-container>
+    <template slot="header">
+      <el-row :gutter="20">
+        <el-col :span="7">
+          <!-- 顶部两个按钮 -->
+          <el-button-group>
+            <!-- 顶部两个按钮-其一 -->
+            <el-tooltip
+              content="新物品购置资金申请"
+              placement="bottom">
+              <el-button
+                size="medium"
+                type="primary"
+                @click="dialogNewItemArouseChangesNumber++">
+                物品申请
+              </el-button>
+            </el-tooltip>
+            <!-- 顶部两个按钮-其二 -->
+            <el-tooltip
+              content="物品损坏上报"
+              placement="bottom">
+              <el-button
+                size="medium"
+                type="primary"
+                @click="dialogOldItemArouseChangesNumber++">
+                损坏上报
+              </el-button>
+            </el-tooltip>
+          </el-button-group>
+        </el-col>
+        <el-col :span="9"><el-input v-model="inputSearch" placeholder="请输入搜索内容"></el-input></el-col>
+      </el-row>
+    </template>
+
+    <!-- 表格 -->
+    <ItemTable/>
+
+    <!-- 底部分页 -->
+    <template slot="footer">
+      <el-row :gutter="20" type="flex" class="row-bg" justify="space-around">
+        <el-col :span="7">
+          <div class="block">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page.sync="currentPage"
+              :page-size="30"
+              layout="prev, pager, next, jumper"
+              :total="600">
+            </el-pagination>
+          </div>
+        </el-col>
+      </el-row>
+    </template>
+    <!-- 顶部两个按钮其一换出的 dialog -->
+    <DialogOfNewItem :dialogArouse="dialogNewItemArouseChangesNumber"/>
+    <!-- 顶部两个按钮其二换出的 dialog -->
+    <DialogOfOldItem :dialogArouse="dialogOldItemArouseChangesNumber"/>
+  </d2-container>
+</template>
+
+<script>
+import DialogOfNewItem from './components/DialogOfNewItem'
+import DialogOfOldItem from './components/DialogOfOldItem'
+import ItemTable from './components/ItemTable'
+export default {
+  name: 'FixedAssetManagement-ItemManagement',
+  components: {
+    DialogOfNewItem,
+    DialogOfOldItem,
+    ItemTable
+  },
+  data () {
+    return {
+      dialogNewItemArouseChangesNumber: 0,
+      dialogOldItemArouseChangesNumber: 0,
+      // 搜索栏的值
+      inputSearch: '',
+      // 分页跳转
+      currentPage: 1
+    }
+  },
+  methods: {
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+    }
+  }
+}
+</script>
+
+<style scoped less="">
+.el-row {
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+    margin-bottom: 0;
+  }
+.el-col {
+  border-radius: 4px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc00;
+}
+.block {
+  border-radius: 3px;
+}
+</style>
