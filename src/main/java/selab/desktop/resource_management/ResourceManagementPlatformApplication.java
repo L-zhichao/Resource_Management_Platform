790f68addde3e4919c291d8c5f47bcf37962cacf
MrTry-Zhao <117160816+MrTry-Zhao@users.noreply.github.com>
2023-07-22 17:42:35 +0800

    前端接口文档配置
    前端接口文档配置

--- src/main/java/selab/desktop/resource_management/ResourceManagementPlatformApplication.java
+++ src/main/java/selab/desktop/resource_management/ResourceManagementPlatformApplication.java
@@ -1,9 +1,11 @@
 package selab.desktop.resource_management;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ResourceManagementPlatformApplication {
 
     public static void main(String[] args) {
