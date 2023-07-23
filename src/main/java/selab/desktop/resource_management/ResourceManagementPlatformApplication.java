bcd7b38dc4af5364f85c5244341a5c73240e97c5
MrTry-Zhao <117160816+MrTry-Zhao@users.noreply.github.com>
2023-07-23 09:28:38 +0800

    增加日志功能
    add slf4j

--- src/main/java/selab/desktop/resource_management/ResourceManagementPlatformApplication.java
+++ src/main/java/selab/desktop/resource_management/ResourceManagementPlatformApplication.java
@@ -1,14 +1,16 @@
 package selab.desktop.resource_management;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@Slf4j
 public class ResourceManagementPlatformApplication {
 
     public static void main(String[] args) {
+        log.info("程序启动");
         SpringApplication.run(ResourceManagementPlatformApplication.class, args);
     }
 
--- src/main/java/selab/desktop/resource_management/fundManagement/controller/FundsController.java
+++ src/main/java/selab/desktop/resource_management/fundManagement/controller/FundsController.java
@@ -3,6 +3,7 @@ package selab.desktop.resource_management.fundManagement.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import selab.desktop.resource_management.fundManagement.domain.Vo.FundsVo;
@@ -12,6 +13,7 @@ import selab.desktop.resource_management.fundManagement.utils.R;
 import java.util.Date;
 import java.util.List;
 
+@Slf4j
 @RestController
 @Tag(name = "资金管理controller")
 @RequestMapping("/fundsVo")
@@ -22,6 +24,7 @@ public class FundsController {
     @GetMapping("/fundsVo")
     @Operation(summary = "展示目前总资产")
     public R<List<FundsVo>> getAllFunds(){
+        log.info("展示目前总资产");
         return R.success(fundsService.list());
     }
 
@@ -31,6 +34,7 @@ public class FundsController {
         QueryWrapper<FundsVo> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("judge", 1);
         List<FundsVo> list = fundsService.list(queryWrapper);
+        log.info("展示所有可支配资产");
         return R.success(list);
     }
 
@@ -41,10 +45,12 @@ public class FundsController {
         if (fundsVo.getId() == null) {
             fundsVo.setUpdateTime(new Date());
             fundsService.save(fundsVo);
+            log.info("增加资金");
             return R.success("增加成功");
         } else {
             fundsVo.setUpdateTime(new Date());
             fundsService.updateById(fundsVo);
+            log.info("更新资金");
             return R.success("更新成功");
         }
     }
@@ -52,6 +58,7 @@ public class FundsController {
     @Operation(summary = "根据主键ID删除某项资金")
     public R<String> delete(@PathVariable Long id) {
         fundsService.removeById(id);
+        log.info("根据主键ID删除某项资金");
         return R.success("删除成功");
     }
 
