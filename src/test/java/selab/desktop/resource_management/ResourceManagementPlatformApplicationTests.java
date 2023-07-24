2409eb5bc40c1eed240d6d4e913b74ca03c893b6
MrTry-Zhao <117160816+MrTry-Zhao@users.noreply.github.com>
2023-07-24 10:18:04 +0800

    add test
    add test

--- src/test/java/selab/desktop/resource_management/ResourceManagementPlatformApplicationTests.java
+++ src/test/java/selab/desktop/resource_management/ResourceManagementPlatformApplicationTests.java
@@ -1,14 +1,26 @@
 package selab.desktop.resource_management;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import selab.desktop.resource_management.userManagement.controller.UserController;
+import selab.desktop.resource_management.userManagement.domain.vo.UserVo;
 import selab.desktop.resource_management.userManagement.mapper.UserMapper;
+import selab.desktop.resource_management.userManagement.service.impl.UserServiceImpl;
 
 @SpringBootTest
 class ResourceManagementPlatformApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Test
+    public void register001(){
+        UserController userController = new UserController();
+        UserVo userVo = new UserVo();
+
+        userController.register(userVo);
+    }
 
 
 }
