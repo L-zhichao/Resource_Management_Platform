6e9eb9e23b445f1211cc46fb9d1d25ff37d2e940
XunJi <329453626x@gmail.com>
2023-07-30 13:26:57 +0800

    支持视频播放

--- src/main/java/selab/desktop/resource_management/views/d2-admin-master/src/views/FixedAssetManagement/ItemManagement/components/videoPlayer/index.vue
+++ src/main/java/selab/desktop/resource_management/views/d2-admin-master/src/views/FixedAssetManagement/ItemManagement/components/videoPlayer/index.vue
@@ -1,11 +1,13 @@
 <template>
 <el-dialog
-  title="提示"
   :visible.sync="dialogVisible"
-  width="30%">
-  <span>这是一段信息</span>
-  <span slot="footer" class="dialog-footer">
-  </span>
+  width="80%">
+  <video
+    :src="videoUrl"
+    controls
+    class="video"
+    width="100%">
+  </video>
 </el-dialog>
 </template>
 
@@ -14,6 +16,9 @@ export default {
   name: 'VideoPlayer',
   props: {
     dialogVideoPlayer: {
+      required: true
+    },
+    videoUrl: {
       required: true
     }
   },
@@ -25,6 +30,7 @@ export default {
   watch: {
     dialogVideoPlayer () {
       this.dialogVisible = true
+      console.log(this.videoUrl)
     }
   }
 }
