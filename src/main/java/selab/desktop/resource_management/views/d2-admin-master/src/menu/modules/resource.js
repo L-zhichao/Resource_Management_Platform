7d5b34c70773c8ab44f539d340675109989f7f9c
lizhichao <2658085067@qq.com>
2023-08-09 08:54:22 +0800

    合并代码

--- src/main/java/selab/desktop/resource_management/views/d2-admin-master/src/menu/modules/resource.js
+++
@@ -1,9 +0,0 @@
-export default {
-  path: 'resource',
-  title: '云端',
-  icon: 'cloud',
-  children: [
-    { path: '/ResourceTest', name: 'ResourceTest', title: '云', icon: 'cloud' },
-    { path: '/UpLoad', name: 'Upload', title: '云上传', icon: 'cloud' }
-  ]
-}
